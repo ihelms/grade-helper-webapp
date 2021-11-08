ac37dc675dae42dbb87df8cdf66cc2b98e2dee09
ihelms <ihelms@united-internet.de>
2021-11-08 14:56:15 +0100

    + added Application.java + added to .gitignore + added pom.xml

---
+++ src/main/java/com/grade/helper/Application.java
@@ -0,0 +1,16 @@
+package com.grade.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * created by ihelms on 08.11.2021
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
